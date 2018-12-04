05a096101c3c28cef88b2a9e362d210c99abef34
An Nguyen <32471775+annguyen997@users.noreply.github.com>
2018-12-03 21:05:28 -0500

    Add files via upload

--- Department.java
+++ Department.java
@@ -1,30 +1,56 @@
+/**
+ * The Department class that contains department name, name of manager - an Employee object,
+ * and description of the department
+ *
+ * @author An Nguyen
+ */
 public class Department {
+    /* The name of the department */
     private String name;
+
+    /* The Employee object that is designated as manager of department */
     private Employee manager;
+
+    /* The description of the department */
     private String description;
 
     /* The number of employees in this department
     * This may need to be reflected in requirements logs*/
     private int numberOfEmployees;
 
-    /* The number of Department objects available
+    /* The number of Department objects available in the application (i.e. company)
      * This may need to be reflected in requirements logs*/
     private static int numberOfDepartments;
 
+    /* Default Department constructor */
     public Department(){
     }
 
+    /** Department constructor with department name provided
+     * @param name - name of the department
+     * */
     public Department(String name){
         this.name = name;
         numberOfDepartments++;
     }
 
+    /** Department constructor with department name and manager provided
+     *
+     * @param name - name of the department
+     * @param manager - manager of the department
+     * */
     public Department(String name, Employee manager){
         this.name = name;
         this.manager = manager;
         numberOfDepartments++;
     }
 
+    /** Department constructor with department name, department description, and manager provided
+     *
+     * @param name - name of the department
+     * @param manager - manager of the department
+     * @param description - description of the department
+     * */
     public Department(String name, Employee manager, String description){
         this.name = name;
         this.manager = manager;
@@ -32,30 +58,59 @@ public class Department {
         numberOfDepartments++;
     }
 
+    /** Get the name of department
+     *
+     *
+     * @return name of the department
+     */
     public String getName() {
         return name;
     }
 
+    /** Set the name of department
+     *
+     * @param name - Name of the department
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /** Get the manager Employee object of department
+     *
+     * @return Employee object that is manager of the department
+     */
     public Employee getManager() {
         return manager;
     }
 
+    /** Set the manager Employee object of department
+     *
+     * @param manager The manager of the department
+     */
     public void setManager(Employee manager) {
         this.manager = manager;
     }
 
+    /** Get the description of the department
+     *
+     * @return the description of the department
+     */
     public String getDescription() {
         return description;
     }
 
+    /** Set the description of the department
+     * @return return the description of the department
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /** Print the department and its attributes
+     *
+     *
+     * @return A string containing the department's attributes
+     */
     @Override
     public String toString() {
         return "Department{" +
--- HRSystem.java
+++ HRSystem.java
@@ -1,6 +1,32 @@
+import javax.swing.JOptionPane;
+
 public class HRSystem {
 
-    public static void addEmployee(){};
+    public static void addEmployee(){
+        String firstName = JOptionPane.showInputDialog("Enter first name of employee: ");
+        String lastName = JOptionPane.showInputDialog("Enter last name of employee: ");
+        String status = JOptionPane.showInputDialog("Is " + firstName + " " + lastName + " a part-time or full-time employee?: ");
+        double salary = Integer.parseInt(JOptionPane.showInputDialog("Enter the salary?: "));
+        String title = JOptionPane.showInputDialog("Enter the title of position: ");
+
+        /* String insurance = JOptionPane.showInputDialog("What is the insurance of + " firstName + " "
+                        + lastName + "?"); */
+        /* String department = JOptionPane.showInputDialog("Enter department of + firstName + " " + lastName + ": "); */
+
+        /* For part-time employees, this will instead indicate the hourly wage */
+        if (status == "Part-Time"){
+            
+        }
+        protected double salary;
+        protected String firstName;
+        protected String lastName;
+        /* Status indicates if the employee is a part-time or full-time employee */
+        protected String status;
+        protected Insurance insurance;
+        protected Department department;
+        protected String title;
+
+    };
     public static Employee removeEmployee(String employeeID){ return null;};
     public static void editEmployee(String employeeID){};
     public static String displayEmployees(){ return null;};
@@ -10,4 +36,9 @@ public class HRSystem {
     public static void departmentSort(){};
     public static void insuranceAmountSort(){};
     public static void salaryAmountSort(){};
+
+    /* Other methods */
+    public static void createDepartment(){
+
+    }
 }

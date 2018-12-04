7505afa98f41307111f93309a58cfc259f8d42a1
Han Jang <hanbitj14@gmail.com>
2018-12-04 00:51:09 -0500

    changes

--- HRSystem.java
+++ HRSystem.java
@@ -4,31 +4,63 @@ public class HRSystem {
 
     public static void main(String[] args)
     {
-      
+      boolean isDone = false;
+      String promptMsg = "HR System\nList of service(1-5)\n1. Add New Employee\n2. Remove Employee\n3. Edit Employee Info\n4. Display All\n5. Exit\n";
+      int chosenService; 
+        
+      while(!isDone)
+      {
+         try{
+            chosenService = Integer.parseInt(JOptionPane.showInputDialog(null, promptMsg));
+            if(chosenService < 1 || chosenService > 5)
+            {
+               throw new IllegalArgumentException("Invalid Range");
+            }
+            if(chosenService == 1)
+            {
+               addEmployee();
+            }
+            if(chosenService == 2)
+            {
+               removeEmployee();
+            }
+            if(chosenService == 3)
+            {
+               editEmployee();
+            }            
+            if(chosenService == 4)
+            {
+               displayAll();
+            }            
+            if(chosenService == 5)
+            {
+               isDone = true;
+            }
+         }
+         catch(NumberFormatException e)
+         {
+            JOptionPane.showMessageDialog(null, "Invalid Range.");
+         }                     
+         catch(IllegalArgumentException e)
+         {
+            JOptionPane.showMessageDialog(null, e);
+         }
+      } 
     }
 
     /** Add employee method if system needs to add a new employee not originally found in text file */
     public static void addEmployee(){
-        String firstName = JOptionPane.showInputDialog("Enter first name of employee: ");
-        String lastName = JOptionPane.showInputDialog("Enter last name of employee: ");
-        String status = JOptionPane.showInputDialog("Is " + firstName + " " + lastName + " a part-time or full-time employee?: ");
-        double salary = Integer.parseInt(JOptionPane.showInputDialog("Enter the salary?: "));
-        String title = JOptionPane.showInputDialog("Enter the title of position: ");
+    }
 
-        /* String insurance = JOptionPane.showInputDialog("What is the insurance of + " firstName + " "
-                        + lastName + "?"); */
-        /* String department = JOptionPane.showInputDialog("Enter department of + firstName + " " + lastName + ": "); */
+    public static void removeEmployee(){
+    }    
 
-        /* For part-time employees, this will instead indicate the hourly wage */
-        if (status == "Part-Time"){
-            Employee partTimeEmployee = new PartTimeEmployee();
-        } else {
-            Employee employee = new Employee();
-        }
-    };
-    public static Employee removeEmployee(String employeeID){ return null;};
-    public static void editEmployee(String employeeID){};
-    public static String displayEmployees(){ return null;};
+    public static void editEmployee(){
+    }    
+
+    public static void displayAll(){
+    }    
+
 
     /* Sorting methods */
     public static void alphabeticSort(){};

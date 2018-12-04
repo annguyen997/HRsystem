f93a921a43a48960e5c34343a4854d1f0e2e1ce6
An Nguyen <32471775+annguyen997@users.noreply.github.com>
2018-12-04 02:06:31 -0500

    Update Insurance.java

--- Insurance.java
+++ Insurance.java
@@ -1,8 +1,6 @@
 public class Insurance {
     private String planName;
     private int amount;
-    /* Indicates the number of people using this plan. This may need to be reflected in the requirements log*/
-    private int members;
 
     public Insurance(String planName, int amount){
         this.planName = planName;
@@ -25,11 +23,7 @@ public class Insurance {
     public void setPlanName(String planName) {
         this.planName = planName;
     }
-
-    public int getMembers() {
-        return members;
-    }
-
+    
     @Override
     public String toString() {
         return "Insurance{" +
